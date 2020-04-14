f5c8aff6ea325f82d33af3dcd04d2d5608e3feb8
ing <inyoman_gurnitha@yahoo.com>
2020-04-15 00:46:38 +0700

    281. Use CSS in Gatsby

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,12 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to Anjel's Restaurant.</p>
-    <p>
-      Great and deliciouse Japanese and Korean Food cooks by a Balinese women.
-    </p>
-    <p>
-      279. Deploy On Netlify.
-    </p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <SEO title="Home" keyword={[`gatsby`, `application`, `react`]} />
+    <h3>Hello, Welcome to Anjel's Restaurant</h3>
   </Layout>
 )
 
